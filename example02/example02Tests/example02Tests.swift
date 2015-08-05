9aa4c50964441bfdf751c07f543f2bf327697038
changqing <chenchangqing198@126.com>
2015-08-05 10:30:53 +0800

    initial example02

---
+++ example02/example02Tests/example02Tests.swift
@@ -0,0 +1,36 @@
+//
+//  example02Tests.swift
+//  example02Tests
+//
+//  Created by green on 15/8/5.
+//  Copyright (c) 2015年 com.chenchangqing. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class example02Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
